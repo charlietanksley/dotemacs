0ce68bd2856357bb42eb5437374ce3854ac2275a
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-10-22 20:41:14 -0400

    move init.org stuff out into init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,39 @@
-(require 'org)
-(org-babel-load-file "my-init.org")
+;;;;;;;;;;;;;;;;;;
+;; Basic config ;;
+;;;;;;;;;;;;;;;;;;
+
+;;; Sounds, colors, and shapes (layout)
+
+;; A bell is annoying to everyone.
+(setq visible-bell 1)
+
+; Use as much screen real estate as possible for actual content.
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;;;;;;;;;;;
+;; Files ;;
+;;;;;;;;;;;
+
+;; Backups aren't super-helpful
+(setq make-backup-files nil)
+
+;;;;;;;;;;;;;;;;
+;; Characters ;;
+;;;;;;;;;;;;;;;;
+
+;; Tabs and spaces
+(setq-default indent-tabs-mode nil)
+(setq standard-indent 2)
+
+;;;;;;;;;;;
+;; Modes ;;
+;;;;;;;;;;;
+
+;;; Org mode
+
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
+
+
+
