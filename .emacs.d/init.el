e3d25ecafd7dbd8cf00386e81f4d240f3de87144
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-10-31 21:34:06 -0400

    bring in ack-and-a-half

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -72,6 +72,15 @@
 ;; Modes and packages ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;; Ack
+(use-package ack-and-a-half
+  :config
+  (progn
+    (defalias 'ack 'ack-and-a-half)
+    (defalias 'ack-same 'ack-and-a-half-same)
+    (defalias 'ack-find-file 'ack-and-a-half-find-file)
+    (defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)))
+
 ;;; Circe
 (use-package circe)
 
