96551c7dd6a28c851a84b2858552bc3bf21bc5d6
Charlie Tanksley <charlie.tanksley@gmail.com>
2017-10-19 09:41:19 -0400

    changes the order of lookup for package repos

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,9 +4,9 @@
 
 (require 'cl)
 (require 'package)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")))
 
 (package-initialize)
 (if (not (package-installed-p 'use-package))
