fd097d5934717bc42f704e7bcb31d4d9370e7315
Charlie Tanksley <charlie.tanksley@gmail.com>
2017-10-19 18:56:42 -0400

    uses flx for counsel-M-x
    This is a fuzzy matching.  TBH I'm not sure I love it.  Mostly I'm
    missing the sorting that ido did, I think.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -108,14 +108,20 @@
   :ensure t)
 (use-package counsel
   :ensure t)
+(use-package flx
+  :ensure t)
 (use-package ivy
   :ensure t
   :init (ivy-mode 1)
   :bind (("C-s" . swiper)
-	     ("M-x" . counsel-M-x)
-	     ("C-x C-f" . counsel-find-file)
-	     ("C-c C-r" . ivy-resume))
+         ("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("C-c C-r" . ivy-resume))
   :config (progn
+            (setq ivy-re-builders-alist
+                  '((ivy-switch-buffer . ivy--regex-plus)
+                    (t . ivy--regex-fuzzy)))
+            (setq ivy-initial-inputs-alist nil)
             (setq ivy-use-virtual-buffers t)
             (setq ivy-count-format "(%d/%d) ")))
 
